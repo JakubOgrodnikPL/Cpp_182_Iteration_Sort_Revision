0ade5edc3eb00ba53b0eeff519208674abb22674
Jakub Ogrodnik <jakub.ogrodnik.pl@gmail.com>
2023-04-15 08:13:56 +0200

    Dodaj pliki projektów.

---
+++ Cpp_182_Iteration_Sort_Revision/Cpp_182_Iteration_Sort_Revision.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void recursiveInsertionSort(vector<int>& arr, int n)
+{
+	if (n <= 1) {
+		return;
+	}
+
+	recursiveInsertionSort(arr, n - 1);
+	int last = arr[n - 1];
+	int j = n - 2;
+
+	while (j >= 0 && arr[j] > last)
+	{
+		arr[j + 1] = arr[j];
+		j--;
+	}
+
+	arr[j + 1] = last;
+}
+
+void printArray(vector<int>& arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	vector<int> arr = { 12, 11, 13, 5, 6 };
+	int n = arr.size();
+
+	recursiveInsertionSort(arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
